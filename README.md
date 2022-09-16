ce702b1ce6e502c377f1b3009e8ddbb8754c21aa
AshS <emailash62@gmail.com>
2022-09-16 08:42:03 +0800

    Updating with latest local version

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # work-day-scheduler
+
+I have created a work day scheduler which uses Javascript to create HTML. 
+
+The JS creates time blocks with the following functions:
+1. time blocks are set to work hours
+2. the color changes depending on whether it is past present or future
+3. there is a running clock displaying local time at the top of the page
+
+Here is a screenshot of the page.
+
+
+
+Link to my Github: https://github.com/Web3Zero/work-day-scheduler
+
+Link to the Work Day Scheduler: 
+
+
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,59 +1,80 @@
 
 
-
-function createTimeBlock(time, content="") {
+function createTimeBlock(time, content = "") {
 
     const timeRow = $("<div class='row align-items-center time-row'>")
-    
+
     const colTime = $("<div class='col-2 d-flex justify-content-center time-col-time'>").text(time + ":00")
     const colTextArea = $("<div class='col-8 time-col-textarea'>")
     const textArea = $("<textarea cols='30' rows='2'>").val(content)
 
-// colours will change depending on whether the time is in the past, present or future
 
+    // colours will change depending on whether the time is in the past, present or future
+    // if currentTime > time but < time + 1 -- present
+    const timeMoment = moment(time, "H");
+    const currentTime = moment();
+    const isPresent = (currentTime > timeMoment) && (currentTime < timeMoment.clone().add(1, 'hours'));
+    const isPast = (timeMoment < currentTime);
+    const isFuture = (timeMoment > currentTime);
+
+    // functions to check if the time is past, present or future which then applies the styling from the CSS
+    if (isPresent) {
+        textArea.addClass("present");
+    }
+    if (isPast) {
+        textArea.addClass("past");
+    }
+    if (isFuture) {
+        textArea.addClass("future");
+    }
 
     colTextArea.append(textArea);
 
     const colButton = $("<div class='col-2 time-col-button'>")
-    const button = $('<button type="submit" class="btn btn-primary">').text("save")
+    const button = $('<button type="submit" class="btn btn-primary">')
+    button.text("save")
+    button.on("click", function () {
+        console.log("test");
+        console.log($(this).parent().prev().children("textarea").val());
+        const task = $(this).parent().prev().children("textarea").val();
+    })
     colButton.append(button);
+
+
 
     return timeRow.append(colTime, colTextArea, colButton);
 }
 
 // when user lands on this app should see clock ticking
-$(function(){
+$(function () {
     const currentTime = $("#current-time");
     const container = $(".container")
-    
-    function startTimer(){
 
-        setInterval(function(){    
-        currentTime.text(moment().format("DD-MMM-YYYY hh:mm:ssa"));
+    function startTimer() {
 
-    }, 1000);
-}
+        setInterval(function () {
+            currentTime.text(moment().format("DD-MMM-YYYY hh:mm:ssa"));
 
-
-
-startTimer();
+        }, 1000);
+    }
 
 
 
+    startTimer();
 
 
-// user can type into any of the boxes and save the entry.
-// note should be saved to storage (based on the time of the row)
+    // when a user lands on the page should see 9am to 5pm blocks with all the previous notes
+    for (let index = 9; index < 18; index++) {
+
+        const previousNotes = "";
+        const timeBlock = createTimeBlock(index, "");
+
+        container.append(timeBlock);
+    }
 
 
-// when a user lands on the page should see 9am to 5pm blocks with all the previous notes
-for (let index = 9; index < 18; index++) {
 
-    const previousNotes = "";
-    const timeBlock = createTimeBlock(index, "Hey");
-    
-    container.append(timeBlock);
-}
-
+    // user can type into any of the boxes and save the entry.
+    // note should be saved to storage (based on the time of the row)
 
 })
